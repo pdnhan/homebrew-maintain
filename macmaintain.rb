932c08855c91b3b0d68041d943fbab56f11e9e52
Danh Nhan Phung <nhan.phung@itgrate.com>
2026-04-01 20:34:00 +0700

    chore: update macmaintain to v1.0.5

--- macmaintain.rb
+++ macmaintain.rb
@@ -1,8 +1,8 @@
 class Macmaintain < Formula
   desc "Mac maintenance and optimization checker"
   homepage "https://github.com/pdnhan/mac-maintenance-script"
-  url "https://github.com/pdnhan/mac-maintenance-script/archive/refs/tags/v1.0.3.tar.gz"
-  sha256 "e013f61b88c0ebb79be73430a4fd76750ec66e65bd7acd5009390b21920dbe04"
+  url "https://github.com/pdnhan/mac-maintenance-script/archive/refs/tags/v1.0.5.tar.gz"
+  sha256 "f6ca4a3740f1369999e243148c5a689d3fd336dcfa19fa7d9db73224d8e8140b"
   license "MIT"
 
   def install
